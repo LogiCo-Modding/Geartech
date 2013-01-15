f202189d2830d970d8b4b68d1ed48588b3b112cd
floppygunk <floppygunk@gmail.com>
2013-01-16 10:13:50 +1300

    Deleting files so I can revert the delete (this is crazy...)
    Netbeans screws up when it comes to Git. Don't EVER use Git with
    Netbeans.

--- geartech/gear/tileentities/TileEntities.java
+++
@@ -1,9 +0,0 @@
-package gearteam.geartech.gear.tileentities;
-
-import cpw.mods.fml.common.registry.GameRegistry;
-
-public class TileEntities {
-    public static void load() {
-        GameRegistry.registerTileEntity(TileEntityShaft.class, "Shaft");
-    }
-}
